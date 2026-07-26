a5ea6b5ef20feff0bf612fd58df4fdfa8d5766be
Rubendvbarreto <rubenvasquez556@gmail.com>
2026-07-26 03:57:20 -0300

    chore: add eslint disable rule to DefaultTheme interface in styled-components declaration

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -2,8 +2,9 @@ import 'styled-components';
 
 import theme from './theme';
 
-type ThemeType = typeof theme
+type ThemeType = typeof theme;
 
 declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
 };
