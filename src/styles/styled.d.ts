29c964d2544efd94d38aca269e9551011e1ee5fb
Rubendvbarreto <rubenvasquez556@gmail.com>
2026-07-26 02:39:49 -0300

    fix: remove obsolete file to resolve build path conflicts

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,8 +1,9 @@
-import theme from './src/styles/theme'
+import 'styled-components'
+
+import theme from './theme'
 
 type ThemeType = typeof theme
 
 declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
 }
